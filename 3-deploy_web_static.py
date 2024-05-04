a07f3637fb6a642a60ecf1b9e368bc6209c5dc11
root <zentrictech@gmail.com>
2024-05-04 07:54:37 -0700

    AirBnB_clone_v2

--- 3-deploy_web_static.py
+++ 3-deploy_web_static.py
@@ -1,89 +1,54 @@
 #!/usr/bin/python3
 """
-Fabric script (based on the file 2-do_deploy_web_static.py) that creates
-and distributes an archive to your web servers, using the function deploy
+Fabric script based on the file 2-do_deploy_web_static.py that creates and
+distributes an archive to the web servers
+
+execute: fab -f 3-deploy_web_static.py deploy -i ~/.ssh/id_rsa -u ubuntu
 """
-from fabric.api import *
+
+from fabric.api import env, local, put, run
 from datetime import datetime
-import os
-
-
-env.hosts = ['52.91.125.119', '52.86.30.214']
-env.user = 'ubuntu'
-env.key_filename = '~/.ssh/id_rsa'
+from os.path import exists, isdir
+env.hosts = ['54.160.77.90', '10.25.190.21']
 
 
 def do_pack():
-    """Function to compress directory
-    Return archive path on success; None on fail
-    """
-    # Set up datetime
-    now = datetime.now()
-    now = now.strftime('%Y%m%d%H%M%S')
-    archive_path = 'versions/web_static_' + now + '.tgz'
-
-    # Create archive
-    local('mkdir -p versions/')
-    result = local('tar -cvzf {} web_static/'.format(archive_path))
-
-    # Check if archiving was successful
-    if result.succeeded:
-        return archive_path
-    return None
+    """generates a tgz archive"""
+    try:
+        date = datetime.now().strftime("%Y%m%d%H%M%S")
+        if isdir("versions") is False:
+            local("mkdir versions")
+        file_name = "versions/web_static_{}.tgz".format(date)
+        local("tar -cvzf {} web_static".format(file_name))
+        return file_name
+    except:
+        return None
 
 
 def do_deploy(archive_path):
-    """
-    creates and distributes an archive to your web servers
-    """
-    try:
-        # Check if file path exists
-        if not (os.path.exists(archive_path)):
-            return False
-
-        # upload archive to web server tmp directory
-        put(archive_path, '/tmp/')
-
-        # target directory
-        target = archive_path[-18:-4]
-        run('sudo mkdir -p /data/web_static/\
-releases/web_static_{}/'.format(target))
-
-        # uncompress archive and delete .tgz
-        run('sudo tar -xzf /tmp/web_static_{}.tgz -C \
-/data/web_static/releases/web_static_{}/'
-            .format(target, target))
-
-        # delete archive from web server
-        run('sudo rm /tmp/web_static_{}.tgz'.format(target))
-
-        # move files to web_static
-        run('sudo mv /data/web_static/releases/web_static_{}/web_static/* \
-/data/web_static/releases/web_static_{}/'.format(target, target))
-
-        # remove cached data
-        run('sudo rm -rf /data/web_static/releases/\
-web_static_{}/web_static'.format(target))
-
-        # delete pre-existing sym link
-        run('sudo rm -rf /data/web_static/current')
-
-        # create new symbolic link
-        run('sudo ln -s /data/web_static/releases/\
-web_static_{}/ /data/web_static/current'.format(target))
-    except FileNotFoundError:
+    """distributes an archive to the web servers"""
+    if exists(archive_path) is False:
         return False
-
-        # if all ops are done correctly
-    return True
+    try:
+        file_n = archive_path.split("/")[-1]
+        no_ext = file_n.split(".")[0]
+        path = "/data/web_static/releases/"
+        put(archive_path, '/tmp/')
+        run('mkdir -p {}{}/'.format(path, no_ext))
+        run('tar -xzf /tmp/{} -C {}{}/'.format(file_n, path, no_ext))
+        run('rm /tmp/{}'.format(file_n))
+        run('mv {0}{1}/web_static/* {0}{1}/'.format(path, no_ext))
+        run('rm -rf {}{}/web_static'.format(path, no_ext))
+        run('rm -rf /data/web_static/current')
+        run('ln -s {}{}/ /data/web_static/current'.format(path, no_ext))
+        return True
+    except:
+        return False
 
 
 def deploy():
-    """
-    Deploy web static
-    """
+    """creates and distributes an archive to the web servers"""
     archive_path = do_pack()
     if archive_path is None:
         return False
-    success = do_deploy(archive_path)
-    return success
+    return do_deploy(archive_path)
