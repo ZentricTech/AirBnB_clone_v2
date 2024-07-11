fc7add358f6b2fbeb77b65d13073f7f395834a89
root <zentrictech@gmail.com>
2024-07-11 03:12:01 -0700

    AirBnB_clone_v2

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,30 +1,59 @@
 #!/usr/bin/python3
 """
-Fabric script based on the file 1-pack_web_static.py that distributes an
-archive to the web servers
+Fabric script (based on the file 2-do_deploy_web_static.py) that
+creates and distributes an archive to your web servers,
+using the function deploy
 """
+from fabric.api import *
+from datetime import datetime
+import os
 
-from fabric.api import put, run, env
-from os.path import exists
-env.hosts = ['54.89.109.87', '100.25.190.21']
+env.hosts = ['52.91.125.119', '52.86.30.214']
+env.user = 'ubuntu'
+env.key_filename = '~/.ssh/id_rsa'
 
-
+@task
 def do_deploy(archive_path):
-    """distributes an archive to the web servers"""
-    if exists(archive_path) is False:
-        return False
+    """
+    creates and distributes an archive to your web servers
+    """
     try:
-        file_n = archive_path.split("/")[-1]
-        no_ext = file_n.split(".")[0]
-        path = "/data/web_static/releases/"
+        # Check if file path exists
+        if not (os.path.exists(archive_path)):
+            return False
+
+        # upload archive to tmp directory of web server
         put(archive_path, '/tmp/')
-        run('mkdir -p {}{}/'.format(path, no_ext))
-        run('tar -xzf /tmp/{} -C {}{}/'.format(file_n, path, no_ext))
-        run('rm /tmp/{}'.format(file_n))
-        run('mv {0}{1}/web_static/* {0}{1}/'.format(path, no_ext))
-        run('rm -rf {}{}/web_static'.format(path, no_ext))
-        run('rm -rf /data/web_static/current')
-        run('ln -s {}{}/ /data/web_static/current'.format(path, no_ext))
-        return True
-    except:
+
+        # target directory
+        target = archive_path[-18:-4]
+        run('sudo mkdir -p /data/web_static/\
+releases/web_static_{}/'.format(target))
+
+        # uncompress archive and delete .tgz
+        run('sudo tar -xzf /tmp/web_static_{}.tgz -C \
+/data/web_static/releases/web_static_{}/'
+            .format(target, target))
+
+        # delete archive from web server
+        run('sudo rm /tmp/web_static_{}.tgz'.format(target))
+
+        # move files to web_static
+        run('sudo mv /data/web_static/releases/web_static_{}/web_static/* \
+/data/web_static/releases/web_static_{}/'.format(target, target))
+
+        # remove cached data
+        run('sudo rm -rf /data/web_static/releases/\
+web_static_{}/web_static'.format(target))
+
+        # delete pre-existing sym link
+        run('sudo rm -rf /data/web_static/current')
+
+        # create new symbolic link
+        run('sudo ln -s /data/web_static/releases/\
+web_static_{}/ /data/web_static/current'.format(target))
+    except FileNotFoundError:
         return False
+
+        # if all ops are done correctly
+    return True
